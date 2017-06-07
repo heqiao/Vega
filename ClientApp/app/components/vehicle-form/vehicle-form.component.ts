deaeb9539992eb1d92df8cf380b2730d25aa1ded
Qiao He <qiao.he@outdoorcap.com>
2017-06-06 20:20:27 -0500

    Added vehicle form

---
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'vehicle-form',
+    templateUrl: './vehicle-form.component.html',
+    styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent {
+}
