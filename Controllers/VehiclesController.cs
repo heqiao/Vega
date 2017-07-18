a2814df48fc38390891e16b8ec74be1b73dc051a
Qiao He <qiao.he@outdoorcap.com>
2017-07-18 14:38:01 -0500

    CRUD

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -24,10 +24,7 @@ namespace Vega.Controllers
         public async Task<IActionResult> CreateVehicle([FromBody]VehicleResource vehicleResource)
         {
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
-
 
             // Complex business validation goes here
             // if (true)
@@ -48,15 +45,43 @@ namespace Vega.Controllers
         public async Task<IActionResult> UpdateVehicle(int id, [FromBody]VehicleResource vehicleResource)
         {
             if (!ModelState.IsValid)
-            {
                 return BadRequest(ModelState);
-            }
 
             var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
             
             await context.SaveChangesAsync();
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var vehicle = await context.Vehicles.FindAsync(id);
+            if (vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+            
+            return Ok(id);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id) {
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+            if (vehicle == null)
+                return NotFound();
+
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
             return Ok(result);
         }
